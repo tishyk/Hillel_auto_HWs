39d0af7807297822ab144d3e943dc023f13d6d77
дмитрий <rubachek1990@gmail.com>
2023-07-30 14:48:00 +0300

    lesson_4_homework

--- main.py
+++ main.py
@@ -1,91 +1,116 @@
-# lesson_3_homework
-#Порахувати кількість унікальних символів в строці. Якщо їх більше 10 - вивести в консоль True, інакше - False. Строку отримати за допомогою функції input()
-
-homework_3 = set(input("Введите значения:"))
-if len(homework_3) > 10:
-    print(True)
-elif len(homework_3) <= 10:
-    print(False)
-else:
-    print("Введено неверное значение")
+#lesson_4_homework
+# 1. Напишіть цикл, який буде вимагати від користувача ввести слово, в якому є буква "о"
+# (враховуються як великі так і маленькі). Цикл не повинен завершитися, якщо користувач ввів слово без букви "о".
 
 
-# #Set_unordered
-# sel_example = {'test', 1, 2, True, True}
-# print(sel_example)
+# task_1_v1
+task1_str = input("Enter the word with letter 'o' in English or Resend languish:")
+counter = 0
+for element_task1 in task1_str:
+    if element_task1 == "o" or element_task1 == "O" or element_task1 == "о" or element_task1 == "О":
+        counter += 1
+
+
+print(f"Word '{task1_str}' contains {counter} letters 'o'")
+
+
+# task_1_v2
+while True:
+    task1_str = input("To stop enter a word with the letter 'o' English or Resend languish:")
+    if 'o' or 'о' in task1_str.lower():
+        break
+
+counter_en = task1_str.lower().count('o')
+counter_ru = task1_str.lower().count('о')
+total_counter = counter_ru + counter_en
+print(f"Word '{task1_str}' contains {total_counter} letters 'o'")
+
+
+# 2.Є list з даними lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum'].
+# Напишіть код, який свормує новий list (наприклад lst2), який містить лише змінні типу стрінг, які присутні в lst1. Данні в лісті можуть бути будь якими
+
+# task_2 v1
+lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum']
+lst2 = []
+for item in lst1:
+    if type(item) != str:
+        continue
+    else:
+        lst2.append(item)
+print(lst2)
+
+
+
+
+
+# 3. Є ліст з числами, порахуйте сумму усіх ПАРНИХ чисел в цьому лісті
+list_num = [1, 11, 21, 31, 100]
+sum_list = 0
+for num in list_num:
+    if (num % 2) == 0:
+        sum_list += num
+    else:
+        continue
+print(sum_list)
+
+
+
+# test_string = "asd"
+# test_list = ["asd", "my name is Dima", "Python is good"]
+
+# for char in test_string:
+#     if char == "a":
+#         continue
+#     print(char)
+
+# for test_in_list in test_list:
+#     for letter in test_in_list:
+#         print(letter)
+
 #
+# test_dict = {1: "name", 2: "name2"}
+# for dict_element in test_dict.items():
+#     print(dict_element)
+
+
+
+# counter = 1
+
+# while counter <= 10:
+#     print(counter)
+#     counter += 1
+
+
+
+
+# while True:
+#     print(counter)
+#     if counter >= 10:
+#         break
+#     counter += 1
 #
-# list_example = ["test@gmail.com", "test@gmail1.com", "test@gmail.com"]
-# set_from_list = list(set(list_example))
-# print(list_example)
-# print(set_from_list)
-# tuple_from_list = tuple(list_example)
-# print(tuple_from_list)
-# print(list('asd'))
+# test_string = "asd vcx"
+# result_str = ""
+# counter = 0
+# while counter <= len(test_string)-1:
+#     if test_string[counter] == " ":
+#         counter += 1
+#         continue
+#     else:
+#         result_str += test_string[counter]
+#         counter += 1
+# print(result_str)
 
 
-# #tuple_ordered_unchangeable
-# tuple_example = (123, 'apple', True, 123)
-# print(tuple_example)
-# print(tuple_example[0])
 
 
 
-# dict_lesson_3_unordered_changeable
-# employers_dict = {"test@gmail.com": "Test 1",
-#                   "test1@gmail.com": "Test 2",
-#                   "test2@gmail.com": "Test3",
-#                   1: "Test4",
-#                   2: 76}
+# text = input("Enter text:")
+# uniq_symbols = set(text)
 #
-# print(employers_dict)
-# print(employers_dict["test@gmail.com"])
-# print(employers_dict[1])
-# employers_dict[2] = 5
-# employers_dict[1] = "New value"
-# print(employers_dict[2])
-# print(employers_dict[1])
-# print(len(employers_dict)) # Чего длинна
-
-
-
-
-
-
-
-# #List_lesson_3_ordered_changeable
-# list_example_int = [1, 2, 5, 8]
-# list_example_str = ['apple', 'banana']
-# list_example_mix = [1, 'apple']
-# # print(list_example_int)
-# # print(list_example_str)
-# # print(list_example_mix)
-# print(list_example_mix[1])
-# list_example_mix[1] = 'strawberry'
-# print(list_example_mix[1])
-
-
-
-# #lesson_3_if_else
-# first_num = int(input('Enter first num:'))
-# second_num = int(input('Enter second num:'))
-#
-# if first_num > second_num:
+# if len(uniq_symbols) > 10:
 #     print(True)
-#     print("first number more than second number")
-# elif first_num < second_num:
-#     print("second number more than first number")
-#     print(False)
 # else:
-#     print("Equal")
-
-
-
-
-# input_example = input("Enter name:")
-# # example_string = 'Dima' # Зачем это?
-# result = f'Word {input_example} has {len(input_example)} letters'
-# result_format = 'Word {word} has {len_word} letters'.format(word=input_example, len_word=len(input_example))
-# print(result_format)
-
-# print(f'Word {(input_word := input("Enter word:"))}  has {len(input_word)} letters')
+#     print(False)
+#
+# print(len(set(input("Enter text"))) > 10)
