404f89b105e09145d92175a017fe07f6826d8572
дмитрий <rubachek1990@gmail.com>
2023-08-03 22:19:43 +0300

    lesson_5_homework

--- main.py
+++ main.py
@@ -1,116 +1,64 @@
-#lesson_4_homework
-# 1. Напишіть цикл, який буде вимагати від користувача ввести слово, в якому є буква "о"
-# (враховуються як великі так і маленькі). Цикл не повинен завершитися, якщо користувач ввів слово без букви "о".
+# Напишіть функцію, яка приймає два аргументи.
+# Якщо обидва аргумени відносяться до числових типів функція пермножує ці аргументи і повертає результат
+# Якшо обидва аргументи відносяться до типу стрінг функція обʼєднує їх в один і повертає
+# В будь-якому іншому випадку - функція повертає кортеж з двох агрументів
+# Імпортуйте цю функцію, і викличте в іншому файлі
 
 
-# task_1_v1
-task1_str = input("Enter the word with letter 'o' in English or Resend languish:")
-counter = 0
-for element_task1 in task1_str:
-    if element_task1 == "o" or element_task1 == "O" or element_task1 == "о" or element_task1 == "О":
-        counter += 1
-
-
-print(f"Word '{task1_str}' contains {counter} letters 'o'")
-
-
-# task_1_v2
-while True:
-    task1_str = input("To stop enter a word with the letter 'o' English or Resend languish:")
-    if 'o' or 'о' in task1_str.lower():
-        break
-
-counter_en = task1_str.lower().count('o')
-counter_ru = task1_str.lower().count('о')
-total_counter = counter_ru + counter_en
-print(f"Word '{task1_str}' contains {total_counter} letters 'o'")
-
-
-# 2.Є list з даними lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum'].
-# Напишіть код, який свормує новий list (наприклад lst2), який містить лише змінні типу стрінг, які присутні в lst1. Данні в лісті можуть бути будь якими
-
-# task_2 v1
-lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum']
-lst2 = []
-for item in lst1:
-    if type(item) != str:
-        continue
+def home_work_5(first, second):
+    if type(first) == int and type(second) == int:
+        return first * second
+    elif type(first) == str and type(second) == str:
+        return first + second
     else:
-        lst2.append(item)
-print(lst2)
+        return (first, second)
 
 
+if __name__ == '__main__':
+    b = 1
+    print(b)
 
-
-
-# 3. Є ліст з числами, порахуйте сумму усіх ПАРНИХ чисел в цьому лісті
-list_num = [1, 11, 21, 31, 100]
-sum_list = 0
-for num in list_num:
-    if (num % 2) == 0:
-        sum_list += num
-    else:
-        continue
-print(sum_list)
-
-
-
-# test_string = "asd"
-# test_list = ["asd", "my name is Dima", "Python is good"]
-
-# for char in test_string:
-#     if char == "a":
-#         continue
-#     print(char)
-
-# for test_in_list in test_list:
-#     for letter in test_in_list:
-#         print(letter)
-
+# # import import_exampel as ie
+# from import_exampel import get_sum
 #
-# test_dict = {1: "name", 2: "name2"}
-# for dict_element in test_dict.items():
-#     print(dict_element)
-
-
-
-# counter = 1
-
-# while counter <= 10:
-#     print(counter)
-#     counter += 1
-
-
-
+# b = 80
+# print(b)
+# a = 1
+# print(b + a)
 
 # while True:
-#     print(counter)
-#     if counter >= 10:
+#     user_input = input('Input word:')
+#     if "o" in user_input.lower():
 #         break
-#     counter += 1
-#
-# test_string = "asd vcx"
-# result_str = ""
-# counter = 0
-# while counter <= len(test_string)-1:
-#     if test_string[counter] == " ":
-#         counter += 1
-#         continue
 #     else:
-#         result_str += test_string[counter]
-#         counter += 1
-# print(result_str)
+#         print("wrong word")
 
-
-
-
-
-# text = input("Enter text:")
-# uniq_symbols = set(text)
+# user_input = input('Input word:')
+# while "o" not in user_input and "O" not in user_input:
+#     user_input = input("The:")
 #
-# if len(uniq_symbols) > 10:
-#     print(True)
-# else:
-#     print(False)
+# print(f'Yas {user_input}')
+
+
+# def sum(first_num, second_num, third_num = 100):
 #
-# print(len(set(input("Enter text"))) > 10)
+#     return  first_num + second_num +third_num
+#
+# sum_resalt = sum(1, 2)
+# print(sum_resalt)
+
+#
+# def get_sum(*args):
+#     return sum(args)
+# def get_sum_t(num_tupel):
+#     return sum(num_tupel)
+#
+# print(get_sum_t((1,2,3,4,6)))
+#
+# def get_summ_kwargs(**kwargs):
+#     return sum(kwargs.values())
+#
+# resalt_kwargs = get_summ_kwargs(a=5, d=6)
+# print(resalt_kwargs)
+
+# print(get_sum(1, 2, 3))
