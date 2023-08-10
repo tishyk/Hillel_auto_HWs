0df6fb7854ea51f3c3261ed7f24c8e6c32e31f83
дмитрий <rubachek1990@gmail.com>
2023-08-10 15:34:49 +0300

    lesson_7_homework

--- main.py
+++ main.py
@@ -1,76 +1,77 @@
-# Написати функцыю, яка буде рекурсивно вираховувати число фібоначчі.
+# Create a file with numbers (in a few rows)
 #
-# Число фібоначчі - це число, яке дорівнює суммі двох попередніх в послідовності (це і повинно бути в рекурсії). Закінчується на двох одиницях
-
-
-def new_fibanacci_fan(num:int):
-    if num == 1 or num == 2:
-        return 1
-    elif num == 0:
-        return 0
-    else:
-        return new_fibanacci_fan(num-1) + new_fibanacci_fan(num-2)
-
-resalt = new_fibanacci_fan(9)
-print(resalt)
-
-
-
-# def fibonacci_num(numer_fib):
-#     if numer_fib == 1 or numer_fib == 2:
-#         return 1
-#     else:
-#         return fibonacci_num(numer_fib-1) + fibonacci_num(numer_fib-2)
+# Read this file, and print the sum of all numbers (create a new function for it)
 #
-# result = fibonacci_num(9)
-# print(result)
-#
-#
-# def fibonacci_even(n):
-#   if n == 0:
-#     return 0
-#   elif n == 1:
-#     return 1
-#   else:
-#     return fibonacci_even(n - 1) + fibonacci_even(n - 2)
-#
-# print(fibonacci_even(7))
-
-# def  weit_for():
-#     text = input("Enter qwe:")
-#     if text == "qwe":
-#         return text
-#     else:
-#         return weit_for()
-#
+# Use try\except block to avoid other symbols except numbers in the file
 
 
-
-#
-# def  weit_for_1():
-#     while True:
-#      text = input("Enter qwe: ")
-#      if text == "qwe":
-#         return text
-#
-# weit_for_1()
-
-# def add_to_num(namder: int):
-#     print(namder)
-#     if (namder == 500):
-#         return namder
-#     else:
-#         namder += 2
-#         add_to_num(namder)
-# # sys.setrecursionlimit(10500)
-#
-# add_to_num(499)
+with open("num.txt", "w") as file_num:
+    file_num.write('6\n')
+    file_num.write('7\n')
+    file_num.write('8\n')
 
 
-# def sum_to_num(number):
-#     return number + 10
+def home_work_7():
+    try:
+        with open("num.txt", "r") as file_num:
+            result = file_num.readlines()
+            result_array = []
+            total = 0
+        for element in result:
+            result_array.append(element.replace("\n", ""))
+        for element_new in result_array:
+            total += int(element_new)
+        return total
+    except FileNotFoundError:
+        print("The file 'num.txt' does not exist.")
+    except:
+        print("The user added a str to the file")
+
+
+new_hom = home_work_7()
+print(new_hom)
+
+# try:
+#     file = open("new_text.txt")
+#     print(file.readlines())
+# except UnicodeDecodeError:
+#     pass
+# finally:
+#     file.close()
 #
-# updated_nam = lambda number: number + 10
+# with open("new_text.txt", "w") as file:
+#     file.write("asd\n")
+#     file.write("My name Dima\n")
+# with open("new_text.txt", "a") as file:
+#     file.write("asd")
+#     file.write("new data\n")
+# with open("new_text.txt", "r") as file:
+#     result =file.readlines()
 #
-# print(sum_to_num(0))
-# print(updated_nam(0))
+# result_array = []
+# for r in result:
+#     result_array.append(r.replace("\n", ""))
+#
+# print(result_array)
+
+# try:
+#     print(int(input("First"))/int(input("Second")))
+# except ZeroDivisionError:
+#     print("Error")
+# except ValueError:
+#     print("Not int")
+# except:
+#     pass
+# finally:
+#     print("Finally")
+#
+# print("asd")
+
+# while True:
+#     try:
+#         resalt = input("Enter a number: ")
+#         inr_resalt = int(resalt)
+#         print(inr_resalt)
+#         break
+#     except:
+#         continue
